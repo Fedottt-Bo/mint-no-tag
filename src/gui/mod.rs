9d0c580593abc08f16f7bba96b5cce975bdc4936
Borodulin Fedor <fedorbborodulin@gmail.com>
2026-04-08 18:40:23 +0300

    Handle `CTRL+Q`
    From Strappazzon/drg-mint-notag.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2094,6 +2094,11 @@ impl eframe::App for App {
                                 self.focus_search = true;
                             }
                         }
+                        egui::Event::Key { key, modifiers, pressed, .. } => {
+                            if key == &egui::Key::Q && *pressed && modifiers.ctrl {
+                                std::process::exit(0);
+                            }
+                        }
                         _ => {}
                     }
                 }
